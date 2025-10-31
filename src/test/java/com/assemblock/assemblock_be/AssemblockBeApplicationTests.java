cb844d0eaf959e942b6ad308de6dd7ab6e2f92e5
gugc29311 <gugc29311@gmail.com>
2025-10-31 16:15:50 +0900

    feat: 초기 세팅

---
+++ src/test/java/com/assemblock/assemblock_be/AssemblockBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assemblock.assemblock_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssemblockBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
