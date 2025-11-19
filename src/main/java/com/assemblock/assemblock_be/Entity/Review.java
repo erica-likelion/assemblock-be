75f6ef3a3db5a5be5d199221d7700090d952d2d1
gugc29311 <gugc29311@gmail.com>
2025-11-19 16:48:23 +0900

    feat: DB 2차 수정사항 반영

--- src/main/java/com/assemblock/assemblock_be/Entity/Review.java
+++ src/main/java/com/assemblock/assemblock_be/Entity/Review.java
@@ -5,13 +5,20 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.DynamicUpdate;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Table(name = "Reviews")
+@Table(name = "Reviews",
+        indexes = {
+                @Index(name = "idx_reviewed_id", columnList = "reviewed_id"),
+                @Index(name = "idx_project_id", columnList = "project_id")
+        }
+)
+@DynamicUpdate
 public class Review {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,15 +26,18 @@ public class Review {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", nullable = false)
+    @JoinColumn(name = "user_id", nullable = false,
+            foreignKey = @ForeignKey(name = "FK_Reviews_user_id"))
     private User user;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "reviewed_id", nullable = false)
-    private User reviewer;
+    @JoinColumn(name = "reviewed_id", nullable = false,
+            foreignKey = @ForeignKey(name = "FK_Reviews_reviewed_id"))
+    private User reviewedUser;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "project_id", nullable = false)
+    @JoinColumn(name = "project_id", nullable = false,
+            foreignKey = @ForeignKey(name = "FK_Reviews_project_id"))
     private Project project;
 
     @Column(name = "review", nullable = false, length = 2048)
