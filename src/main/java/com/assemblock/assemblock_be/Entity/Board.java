98cc7775af832305c5916f30e10ed6d39670a65c
gugc29311 <gugc29311@gmail.com>
2025-11-19 19:49:55 +0900

    feat: DB 3차 수정사항 반영

--- src/main/java/com/assemblock/assemblock_be/Entity/Board.java
+++ src/main/java/com/assemblock/assemblock_be/Entity/Board.java
@@ -14,7 +14,7 @@ import java.util.List;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Table(name = "Boards",
+@Table(name = "Board",
         uniqueConstraints = {
                 @UniqueConstraint(
                         name = "uk_user_board_name",
