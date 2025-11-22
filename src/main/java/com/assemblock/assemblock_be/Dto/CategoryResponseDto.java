6f38097b0c0ea61b8f5b1c847b6ce0a7f489187f
gugc29311 <gugc29311@gmail.com>
2025-11-23 01:34:11 +0900

    feat: DB 4차 수정사항 반영

--- src/main/java/com/assemblock/assemblock_be/Dto/CategoryResponseDto.java
+++ src/main/java/com/assemblock/assemblock_be/Dto/CategoryResponseDto.java
@@ -1,18 +1,23 @@
 package com.assemblock.assemblock_be.Dto;
 
-import com.assemblock.assemblock_be.Entity.TechPart;
+import com.assemblock.assemblock_be.Entity.Block;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
 @Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CategoryResponseDto {
-    private Long techId;
-    private String techName;
-    private List<SubCategoryDto> subCategories;
+    private String techPart;
+    private List<String> categoryNames;
 
-    public CategoryResponseDto(TechPart techPart, List<SubCategoryDto> subCategories) {
-        this.techId = techPart.getId();
-        this.techName = techPart.getTechName().name();
-        this.subCategories = subCategories;
+    public CategoryResponseDto(Block.TechPart techPart, List<String> categoryNames) {
+        this.techPart = techPart.name();
+        this.categoryNames = categoryNames;
     }
 }
