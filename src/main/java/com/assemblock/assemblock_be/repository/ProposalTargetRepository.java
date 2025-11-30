6cb97eb276efe4d2fee52559ecbf3d277944f433
ahn2y <96456605+ahn2y@users.noreply.github.com>
2025-11-30 09:06:40 +0900

    Delete repository directory

--- src/main/java/com/assemblock/assemblock_be/repository/ProposalTargetRepository.java
+++
@@ -1,8 +0,0 @@
-package com.assemblock.assemblock_be.repository;
-
-import com.assemblock.assemblock_be.Entity.ProposalTarget;
-import com.assemblock.assemblock_be.Entity.ProposalTargetId;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProposalTargetRepository extends JpaRepository<ProposalTarget, ProposalTargetId> {
-}
